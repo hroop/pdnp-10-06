c6bb33bae2b094559dbcaa3fdd0ea7e489e5f8e4
radek <rajkonkret660@gmail.com>
2024-06-10 13:01:43 +0200

    [1.6] update teksty.py

--- 1/teksty.py
+++ 1/teksty.py
@@ -36,3 +36,29 @@ encoded_s = tekst.encode('utf-8')
 print(encoded_s)  # b'Witaj \xc5\x9bwiecie'
 print(type(encoded_s))  # <class 'bytes'>
 print(encoded_s.decode('utf-8'))  # Witaj świecie
+
+imie = 'Radek'
+tekst_format = f"Mam na imię {imie} i lubię Pythona"
+# f - f string - tekst sformatowany
+# w {} wpisujemy zawartość zmiennej
+print(tekst_format)  # Mam na imię Radek i lubię Pythona
+tekst_format_2 = f"\tMam na imię {imie}\n i lubię Pythona.\b"
+print(tekst_format_2)
+# "	  Mam na imię Radek
+#  i lubię Pythona"
+# \t - tabulator
+# \n - nowa linia
+# \b - backspace
+
+starszy = "witaj %s!"  # %s - oczekuje stringa
+print(starszy % imie)  # witaj Radek!
+
+print('Witaj {}!'.format(imie))  # Witaj Radek!
+
+# tekst wielolinijkowy
+print("""Tekst
+wielolinijkowy
+    sformatowany""")
+# "Tekst
+# wielolinijkowy
+#     sformatowany"

ed84cc9bb07b763ce98e3e2ccb74fd25befdb577
radek <rajkonkret660@gmail.com>
2024-06-10 13:06:43 +0200

    [1.7] add formaty.py

---
+++ 1/formaty.py
@@ -0,0 +1,8 @@
+user = 'Tomek'  # str
+wiek = 39  # int
+wersja = 3.900001  # float - liczby zmiennoprzecinkowe
+print(type(wersja))  # <class 'float'>
+liczba = 123456789123  # int
+print(type(liczba))  # <class 'int'>
+
+print('witaj %s masz teraz %d lat')

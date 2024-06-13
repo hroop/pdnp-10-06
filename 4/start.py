1e69bc9caa9bf47a9c5c5af8efecf7b2cbde2bcd
radek <rajkonkret660@gmail.com>
2024-06-13 12:50:27 +0200

    [4.12] add pakiet

---
+++ 4/start.py
@@ -0,0 +1,16 @@
+# import fun3
+import pakiet
+from pakiet import fun
+import pakiet.fun as pk
+
+print(pakiet)  # <module 'pakiet' from 'C:\\Users\\cscomarch\\PycharmProjects\\pdnp-10-06\\pakiet\\__init__.py'>
+# po dodaniu w pliku __init__.py mamy metodę powitanie()
+pakiet.powitanie()
+fun.powitanie()
+fun.info()
+# Cześć
+# Jestem pakietem
+pk.powitanie()
+pk.info()
+# Cześć
+# Jestem pakietem
---
+++ pakiet/fun.py
@@ -0,0 +1,13 @@
+def powitanie():
+    print('Cześć')
+
+
+def info():
+    print("Jestem pakietem")
+
+print(__name__)
+if __name__ == '__main__':
+    print("Radek")
+
+# __main__
+# Radek

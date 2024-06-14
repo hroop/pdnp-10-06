2f37e888bd220538251859362c0fc85f52aa3dc4
radek <rajkonkret660@gmail.com>
2024-06-14 14:08:49 +0200

    [5.10] update baza3.py

--- 5/baza3.py
+++ 5/baza3.py
@@ -14,7 +14,39 @@ try:
     c = conn.cursor()
     print('Baza danych została podłączona')
 
+    insert = '''
+    INSERT INTO hardware (id,name,price) VALUES (1, 'Apple',6999);
+    '''
+    insert_samsung = '''
+        INSERT INTO hardware (id,name,price) VALUES (2, 'Samsung',6999);
+        '''
+    # create - insert
+    # c.execute(insert)
+    # conn.commit()
+    # create - insert
+    # c.execute(insert_samsung)
+    # conn.commit()
 
+    # read - select
+    select = """
+    SELECT * FROM hardware;
+    """
+    for row in c.execute(select):
+        print(row)  # (1, 'Apple', 6999.0)
+
+    # update
+    update = """
+    UPDATE hardware SET price=8999 WHERE id=1;
+    """
+    c.execute(update)
+    conn.commit()
+
+    # delete
+    delete = """
+    DELETE FROM hardware WHERE id=1;
+    """
+    c.execute(delete)
+    conn.commit()
 except sqlite3.Error as e:
     print("Bład podłączania do bazy danych", e)
 finally:
